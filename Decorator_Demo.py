9f51b18c156977510457b962e79d4fea17cf4ce3
gkiri <hkkiri@gmail.com>
2019-04-25 20:29:17 +0200

    Create Decorator_Demo.py

---
+++ Decorator_Demo.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+def not_during_the_night(func):
+    def wrapper():
+        if 7 <= datetime.now().hour < 22:
+            func()
+        else:
+            pass  # Hush, the neighbors are asleep
+    return wrapper
+
+def say_whee():
+    print("Whee!")
+
+say_whee = not_during_the_night(say_whee)
+
+############################################################
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_whee():
+    print("Whee!")
+    
+    
+    
+ So, @my_decorator is just an easier way of saying say_whee = my_decorator(say_whee). It’s how you apply a decorator to a function.
