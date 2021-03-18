5992ff34c84354c784b20e0f276671d33c23c06e
gkiri <hkkiri@gmail.com>
2021-03-18 22:49:17 +0100

    Add files via upload

---
+++ PlayGround/generic.cpp
@@ -0,0 +1,16 @@
+// Type your code here, or load an example.
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int square(int num) {
+    return num * num;
+}
+
+int main()
+{
+    vector<int> temp;
+    cout << "hello" << endl;
+    return 0;
+}
